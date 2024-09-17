af3120561476fa875efe0c75ca04ad4ae8eaca5c
gustavoaraujo06 <gugagrilo07@gmail.com>
2024-09-16 21:45:59 -0300

    componente footer

---
+++ src/components/Footer/footer.tsx
@@ -0,0 +1,15 @@
+import "./footer.css"
+
+export default function Footer(){
+    return (
+        <footer>
+            <div>
+                <img src="mecatus.png" className="footerImage"></img>
+                <h2>Mecatus</h2>
+            </div>
+            <div>
+                <img src="logoBranca.png" className="footerImage"></img>
+            </div>
+        </footer>
+    )
+}
